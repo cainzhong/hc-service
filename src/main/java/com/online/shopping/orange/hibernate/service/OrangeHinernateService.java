babf94e7e06eead63efe6f8f4106d0b8485ef6fb
Cain <cainzhong@aliyun.com>
2015-03-29 15:49:49 +0800

    c

--- src/main/java/com/online/shopping/orange/hibernate/service/OrangeHinernateService.java
+++ src/main/java/com/online/shopping/orange/hibernate/service/OrangeHinernateService.java
@@ -1,9 +1,10 @@
-package com.online.shopping.orange.repository.service;
+package com.online.shopping.orange.hibernate.service;
 
 import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.springframework.stereotype.Service;
 
 import com.online.shopping.orange.domain.UserAccount;
 import com.online.shopping.orange.util.HibernateUtil;
@@ -12,11 +13,12 @@ import com.online.shopping.orange.util.HibernateUtil;
  * @author Cain
  *
  */
-public class OrangeRepository {
+@Service
+public class OrangeHinernateService {
 	public List<UserAccount> findUserAccountByUserName(String username) {
 		Session userAccountSession = HibernateUtil.getSessionFactory().openSession();
 		Transaction userAccountTransaction = userAccountSession.beginTransaction();
-
+	
 		List<UserAccount> userAccountlList = userAccountSession.createQuery("from UserAccount").list();
 
 		userAccountTransaction.commit();
