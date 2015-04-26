eeab279ee0522f126e5846985d7b0a6638c8aacd
Cain <cainzhong@aliyun.com>
2015-04-26 21:15:20 +0800

    create service for shopping cart

--- src/main/java/com/online/shopping/orange/pages/service/ShoppingCartService.java
+++ src/main/java/com/online/shopping/orange/pages/service/ShoppingCartService.java
@@ -1,23 +1,21 @@
 package com.online.shopping.orange.pages.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.online.shopping.orange.domain.OrderedItem;
 import com.online.shopping.orange.domain.UserAccount;
 import com.online.shopping.orange.repository.OrangeRepository;
 
 @Service
-public class LoginService {
+public class ShoppingCartService {
 	@Autowired
 	private OrangeRepository orangeRepository;
 
-	public boolean hasMatchedLoginUser(String userName, String password){
-		UserAccount userAccount=this.orangeRepository.findUserAccountByUserName(userName);
-
-		if(userAccount.getPassword().equals(password)){
-			return true;
-		}else{
-			return false;
-		}
+	public List<OrderedItem> findOrderedItemForUser(String userName){
+		List<OrderedItem> orderedItemList=this.orangeRepository.getOrderedItemForUser(userName);
+		return orderedItemList;
 	}
 }
